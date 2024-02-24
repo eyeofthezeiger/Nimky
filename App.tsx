2379d959118c1d303e6b407e0e8c0c67049875a0
Matt Zeiger <matthew.zeiger47@gmail.com>
2024-02-24 13:52:27 -0500

    gradient background working

--- App.tsx
+++ App.tsx
@@ -5,27 +5,35 @@ import { StyleSheet, Text, View } from 'react-native';
 import StartingScreen from './src/screens/StartingScreen';
 import PlayerScreen from './src/screens/PlayerScreen';
 import GameOverScreen from './src/screens/GameOverScreen';
+import { LinearGradient } from 'expo-linear-gradient';
 
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName='Starting' screenOptions={{ headerShown: false, }}>
-        <Stack.Screen name="Starting" component={StartingScreen} />
-        <Stack.Screen name="Game" component={PlayerScreen} />
-        <Stack.Screen name="GameOver" component={GameOverScreen} />
-      </Stack.Navigator> 
-    </NavigationContainer>
+      
+      <NavigationContainer>
+       
+          <Stack.Navigator initialRouteName='Starting' screenOptions={{ headerShown: false, }}>
+          {/* <LinearGradient
+            colors={['#F87F6F', '#FAB77B', '#FBDC93']}
+            style={styles.container}
+            > */}
+            <Stack.Screen name="Starting" component={StartingScreen} />
+            <Stack.Screen name="Game" component={PlayerScreen} />
+            <Stack.Screen name="GameOver" component={GameOverScreen} />
+            {/* </LinearGradient> */}
+          </Stack.Navigator> 
+      </NavigationContainer>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
   },
 });
--- src/screens/StartingScreen.tsx
+++ src/screens/StartingScreen.tsx
@@ -1,174 +1,138 @@
 import React, { useRef, useState } from 'react';
+import { StyleSheet, Text, TouchableOpacity, SafeAreaView, TextInput, ScrollView, KeyboardAvoidingView, Platform, Modal, View } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 import { StackNavigationProp } from '@react-navigation/stack';
-import { View, Text, TouchableOpacity, StyleSheet, SafeAreaView, TextInput, ScrollView, KeyboardAvoidingView, Platform, Modal } from 'react-native';
 import { RootStackParamList } from '../../navigationTypes';
 
 type StartingScreenNavigationProp = StackNavigationProp<RootStackParamList>;
 
-type StartingScreenProps = {
-    navigation: StartingScreenNavigationProp;
+interface StartingScreenProps {
+  navigation: StartingScreenNavigationProp;
 }
 
 const StartingScreen: React.FC<StartingScreenProps> = ({ navigation }) => {
-    const [playerOneName, setPlayerOneName] = useState('Player 1');
-    const [playerTwoName, setPlayerTwoName] = useState('Player 2');
-    const [modalVisible, setModalVisible] = useState(false);
+  const [playerOneName, setPlayerOneName] = useState('Player 1');
+  const [playerTwoName, setPlayerTwoName] = useState('Player 2');
+  const [modalVisible, setModalVisible] = useState(false);
 
-    const playerOneRef = useRef<TextInput>(null);
-    const playerTwoRef = useRef<TextInput>(null);
+  const playerOneRef = useRef<TextInput>(null);
+  const playerTwoRef = useRef<TextInput>(null);
+
   return (
-    <SafeAreaView style={styles.container}>
+    <LinearGradient
+      colors={['#F87F6F', '#FAB77B', '#FBDC93']}
+      style={styles.container}
+    > 
+      <SafeAreaView style={styles.safeAreaView}>
         <KeyboardAvoidingView
-        style={{ flex: 1 }}
-        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-        keyboardVerticalOffset={Platform.OS === 'ios' ? 64 : 0}
+          style={styles.keyboardAvoidingView}
+          behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+          keyboardVerticalOffset={Platform.OS === 'ios' ? 64 : 0}
         >
-      <ScrollView contentContainerStyle={styles.content}>
-        <Text style={styles.mainTitle}>Nimky</Text>
-        <Text style={styles.subTitle}>Remember your people</Text>
-        {/* <Text style={styles.helpfulText}>
-        Welcome to the Name Chain Game! Your goal is to come up with a person's name that begins with a specific letter. Quick thinking and creativity are your best tools.
-        </Text>
-        <Text style={styles.instructionText}>
-        Here's how to play:
-        </Text>
-        <Text style={styles.instructionText}>
-            - If it's your first turn, start with any person's first name that begins with the given letter.
-        </Text>
-        <Text style={styles.instructionText}>
-            - On subsequent turns, your letters for the first and last name are determined by the last letters of the previous answers.
-        </Text>
-        <Text style={styles.instructionText}>
-            Earning Points:
-        </Text>
-        <Text style={styles.instructionText}>
-            - Earn 5 points if the last name you enter starts with the same letter as the ending of the previous answer's last name.
-        </Text>
-        <Text style={styles.instructionText}>
-            - Otherwise, you earn 3 points for a valid guess.
-        </Text>
-        <Text style={styles.instructionText}>Timer:</Text>
-        <Text style={styles.instructionText}>
-            Each turn has a 30-second timer. Be quick, or the game will be over!
-        </Text>
-        <Text style={styles.instructionText}>
-            Each correctly answered turn will add 5 seconds to the clock!
-        </Text> */}
-        <TouchableOpacity style={styles.button} onPress={() => setModalVisible(true)}>
-            <Text style={styles.buttonText}>How to Play</Text>
-        </TouchableOpacity>
-        <TextInput
-            ref={playerOneRef}
-            style={styles.input}
-            placeholder='Enter Player 1 Name...'
-            onChangeText={setPlayerOneName}
-            onSubmitEditing={() => playerTwoRef.current?.focus()}
-        />
-        <TextInput
-        ref={playerTwoRef}
-            style={styles.input}
-            placeholder='Enter Player 2 Name...'
-            onChangeText={setPlayerTwoName}
-            onSubmitEditing={() => navigation.navigate('Game', {
-                playerOneName,
-                playerTwoName
-            })}
-        />
-      </ScrollView>
-      </KeyboardAvoidingView>
-      <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Game', {
-        playerOneName,
-        playerTwoName
-      })}>
-        <Text style={styles.buttonText}>Start</Text>
-      </TouchableOpacity>
-      <Modal
-        animationType="slide"
-        transparent={true}
-        visible={modalVisible}
-        onRequestClose={() => {
-            setModalVisible(!modalVisible);
-        }}
+          <ScrollView contentContainerStyle={styles.content}>
+            <Text style={styles.mainTitle}>Nimky</Text>
+            <Text style={styles.subTitle}>Remember your people</Text>
+            <TouchableOpacity style={styles.button} onPress={() => setModalVisible(true)}>
+              <Text style={styles.buttonText}>How to Play</Text>
+            </TouchableOpacity>
+            <TextInput
+              ref={playerOneRef}
+              style={styles.input}
+              placeholder='Enter Player 1 Name...'
+              onChangeText={setPlayerOneName}
+              onSubmitEditing={() => playerTwoRef.current?.focus()}
+            />
+            <TextInput
+              ref={playerTwoRef}
+              style={styles.input}
+              placeholder='Enter Player 2 Name...'
+              onChangeText={setPlayerTwoName}
+              onSubmitEditing={() => navigation.navigate('Game', { playerOneName, playerTwoName })}
+            />
+          </ScrollView>
+          <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Game', { playerOneName, playerTwoName })}>
+            <Text style={styles.buttonText}>Start</Text>
+          </TouchableOpacity>
+        </KeyboardAvoidingView>
+        <Modal
+          animationType="slide"
+          transparent={true}
+          visible={modalVisible}
+          onRequestClose={() => setModalVisible(!modalVisible)}
         >
-        <View style={styles.centeredView}>
-        <View style={styles.modalView}>
-        <Text style={styles.modalText}>How to Play</Text>
-        <Text style={styles.modalInstructionText}>
-        Welcome to the Name Chain Game! Your goal is to come up with a person's name that begins with a specific letter. Quick thinking and creativity are your best tools.
-        {"\n\n"}Here's how to play:
-        {"\n"}- If it's your first turn, start with any person's first name that begins with the given letter.
-        {"\n"}- On subsequent turns, your letters for the first and last name are determined by the last letters of the previous answers.
-        {"\n\n"}Earning Points:
-        {"\n"}- Earn 5 points if the last name you enter starts with the same letter as the ending of the previous answer's last name.
-        {"\n"}- Otherwise, you earn 3 points for a valid guess.
-        {"\n\n"}Timer:
-        {"\n"}Each turn has a 30-second timer. Be quick, or the game will be over!
-        {"\n"}Each correctly answered turn will add 5 seconds to the clock!
-        </Text>
-        <TouchableOpacity
-            style={[styles.button, styles.buttonClose]}
-            onPress={() => setModalVisible(!modalVisible)}
-        >
-            <Text style={styles.buttonText}>Close</Text>
-        </TouchableOpacity>
-        </View>
-        </View>
+          <View style={styles.centeredView}>
+            <View style={styles.modalView}>
+              <Text style={styles.modalText}>How to Play</Text>
+              <Text style={styles.modalInstructionText}>
+                Welcome to the Name Chain Game! Your goal is to come up with a person's name that begins with a specific letter. Quick thinking and creativity are your best tools.
+                {"\n\n"}Here's how to play:
+                {"\n"}- If it's your first turn, start with any person's first name that begins with the given letter.
+                {"\n"}- On subsequent turns, your letters for the first and last name are determined by the last letters of the previous answers.
+                {"\n\n"}Earning Points:
+                {"\n"}- Earn 5 points if the last name you enter starts with the same letter as the ending of the previous answer's last name.
+                {"\n"}- Otherwise, you earn 3 points for a valid guess.
+                {"\n\n"}Timer:
+                {"\n"}Each turn has a 30-second timer. Be quick, or the game will be over!
+                {"\n"}Each correctly answered turn will add 5 seconds to the clock!
+              </Text>
+              <TouchableOpacity
+                style={[styles.button, styles.buttonClose]}
+                onPress={() => setModalVisible(!modalVisible)}
+              >
+                <Text style={styles.buttonText}>Close</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
         </Modal>
-    </SafeAreaView>
+      </SafeAreaView>
+    </LinearGradient>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    paddingTop: 20, // Adjust based on your screen's design
-    paddingBottom: 35, // Gives some space at the bottom for the button
+  },
+  safeAreaView: {
+    flex: 1,
+  },
+  keyboardAvoidingView: {
+    flex: 1,
   },
   content: {
     flexGrow: 1,
-    justifyContent: 'space-between',
+    justifyContent: 'center',
     alignItems: 'center',
     paddingTop: 20,
-    paddingBottom: 50 // Centers the titles vertically
+    paddingBottom: 50,
   },
   mainTitle: {
     fontSize: 24,
     fontWeight: 'bold',
-    marginBottom: 10, // Gives some space between the main title and subtitle
+    marginBottom: 10,
   },
   subTitle: {
     fontSize: 18,
-  },
-  helpfulText: {
-    fontSize: 16,
-    textAlign: 'center',
-    marginVertical: 10,
-  },
-  instructionText: {
-    fontSize: 16,
-    textAlign: 'center',
-    marginVertical: 10,
   },
   input: {
     height: 40,
     margin: 12,
     borderWidth: 1,
     padding: 10,
-    width: '80%', // Match the button width for consistency
-    borderRadius: 5, // Optional: round corners
+    width: '80%',
+    borderRadius: 5,
   },
   button: {
-    width: '80%', // Adjust based on your design
-    backgroundColor: '#007bff', // Example button color
-    padding: 15, // Adjust padding for button size
-    borderRadius: 5, // Rounds the corners of the button
+    width: '80%',
+    backgroundColor: '#007bff',
+    padding: 15,
+    borderRadius: 5,
     justifyContent: 'center',
     alignItems: 'center',
+    marginTop: 10, // Added to separate buttons and inputs
   },
   buttonText: {
-    color: 'white', // Button text color
+    color: 'white',
     fontSize: 18,
     fontWeight: 'bold',
   },
@@ -185,10 +149,7 @@ const styles = StyleSheet.create({
     padding: 35,
     alignItems: 'center',
     shadowColor: '#000',
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
+    shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.25,
     shadowRadius: 4,
     elevation: 5,
